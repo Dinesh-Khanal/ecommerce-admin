21c2f18056c224fcc718c185d28515340b465ad7
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-09-01 20:36:37 +0545

    check authentication and made setting for specific store

--- app/(dashboard)/[storeId]/setting/page.tsx
+++ app/(dashboard)/[storeId]/setting/page.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default function SettingPage() {
-  return <div>SettingPage</div>;
+export default async function SettingPage({
+  params,
+}: {
+  params: { storeId: string };
+}) {
+  const { storeId } = params;
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const store = await prisma.store.findFirst({
+    where: { id: storeId, userId },
+  });
+  if (!store) {
+    redirect("/");
+  }
+  return <div>SettingPage of {store.name}</div>;
 }
