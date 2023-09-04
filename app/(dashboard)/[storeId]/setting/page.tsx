4f8426f56b90bb198442be7584af3d91c7017fae
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-09-04 21:14:44 +0545

    setting form created and used in setting page

--- app/(dashboard)/[storeId]/setting/page.tsx
+++ app/(dashboard)/[storeId]/setting/page.tsx
@@ -1,23 +1,35 @@
-import prisma from "@/lib/prisma";
-import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import SettingForm from "@/components/setting-form";
+import { auth } from "@clerk/nextjs";
 
-export default async function SettingPage({
-  params,
-}: {
-  params: { storeId: string };
-}) {
-  const { storeId } = params;
+import prisma from "@/lib/prisma";
+
+import { SettingsForm } from "./components/settings-form";
+
+const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
   const { userId } = auth();
+
   if (!userId) {
     redirect("/sign-in");
   }
+
   const store = await prisma.store.findFirst({
-    where: { id: storeId, userId },
+    where: {
+      id: params.storeId,
+      userId,
+    },
   });
+
   if (!store) {
     redirect("/");
   }
-  return <SettingForm store={store} />;
-}
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
