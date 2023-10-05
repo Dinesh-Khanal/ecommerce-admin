3b512b32b8b4a5f4b7004c1bc49db5a2018646dc
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-10-05 22:35:19 +0545

    row actions added

--- app/(dashboard)/[storeId]/billboard/components/columns.tsx
+++ app/(dashboard)/[storeId]/billboard/components/columns.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
 
 export interface BillboardColumn {
@@ -14,5 +15,20 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   {
     header: "Created Date",
     accessorKey: "createdAt",
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const billboards = row.original;
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => navigator.clipboard.writeText(billboards.id)}
+        >
+          Copy Id
+        </Button>
+      );
+    },
   },
 ];
