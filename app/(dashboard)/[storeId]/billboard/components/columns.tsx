caede52e244355ef8f48119b1e580628bba67400
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-10-06 10:25:54 +0545

    row action updated from button to dropdown

--- app/(dashboard)/[storeId]/billboard/components/columns.tsx
+++ app/(dashboard)/[storeId]/billboard/components/columns.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export interface BillboardColumn {
   id: string;
@@ -22,12 +29,31 @@ export const columns: ColumnDef<BillboardColumn>[] = [
     cell: ({ row }) => {
       const billboards = row.original;
       return (
-        <Button
-          variant="ghost"
-          onClick={() => navigator.clipboard.writeText(billboards.id)}
-        >
-          Copy Id
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(billboards.id)}
+            >
+              {" "}
+              <Copy className="mr-2 h-4 w-4" />
+              Copy ID
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Edit className="mr-2 h-4 w-4" />
+              Update
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash className="mr-2 h-4 w-4" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     },
   },
